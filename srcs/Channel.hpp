985cc3581e85c5ce26a04794f84b93fb60160d41
Juliany Bernardo <julianybernardo@outlook.com>
2025-01-31 00:27:14 -0300

    (feat #6) Basic channel implementation

--- srcs/Channel.hpp
+++ srcs/Channel.hpp
@@ -1,29 +1,29 @@
 #pragma once
+
+#include <map>
+#include <string>
+#include <cstdlib>
+#include <iostream>
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
-#include <cstdlib>
-#include <iostream>
-#include <map>
 #include "Client.hpp"
-
-#include <string>
 
 class Channel {
 	
 	private:
 
-		std::string         _name;          // Channel name (starts with # or &)
-		std::string         _topic;         // Channel topic
-		std::string         _key;           // Channel password (if mode +k is set)
+		std::string      	   _name;       // Channel name (starts with # or &)
+		std::string      	   _topic;      // Channel topic
+		std::string      	   _key;        // Channel password (if mode +k is set)
 		std::map<int, Client*> _members;    // Channel members (fd -> Client*)
 		std::map<int, Client*> _operators;  // Channel operators (fd -> Client*)
 		
 		// Channel modes
-		bool _inviteOnly;      // mode +i
-		bool _topicRestricted; // mode +t
-		bool _hasKey;          // mode +k
-		size_t _userLimit;     // mode +l
+		bool	_inviteOnly;      // mode +i
+		bool	_topicRestricted; // mode +t
+		bool	_hasKey;          // mode +k
+		size_t	_userLimit; 	  // mode +l
 
 	public:	
 
@@ -36,16 +36,16 @@ class Channel {
 		void broadcastMessage(const std::string& message, Client* sender = NULL);
 		
 		// Channel operator operations
-		void setMember(Client* client);
+		// void setMember(Client* client);
 		void setOperator(Client* client);
 		bool isOperator(Client* client) const;
 		
 		// Mode operations
-		//if only operators are able to set modes, a setter function to do it
+		// if only operators are able to set modes, a setter function to do it
 		// is not necessary, checkMode though, is necessary, because other entities
 		// outside the Channel class may neeed to access this information   
 		// void setMode(char mode, bool status);
-		bool checkChannelModes(char mode) const;
+		int checkChannelModes(char mode) const;
 		bool checkUserModes(char mode, Client* client) const;
 		
 		// Getters
@@ -56,5 +56,5 @@ class Channel {
 		// Required by subject
 		bool kickMember(Client* operator_client, Client* target, const std::string& reason);
 		bool inviteMember(Client* operator_client, Client* target);
-		bool setTopic(Client* client, const std::string& new_topic);
+		bool setTopic(Client* operator_client, const std::string& new_topic);
 };
